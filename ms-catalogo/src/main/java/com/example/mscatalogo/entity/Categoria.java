d82d947893049667209fa2d91850cb50d6ad1eeb
Uhziel-1 <uhzielcreciendo@gmail.com>
2025-03-20 08:50:03 -0500

    Avance 1 Lombok Error

--- ms-catalogo/src/main/java/com/example/mscatalogo/entity/Categoria.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/entity/Categoria.java
@@ -1,4 +1,35 @@
 package com.example.mscatalogo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+
+    // Getter para 'id'
+    public Integer getId() {
+        return id;
+    }
+
+    // Setter para 'id'
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // Getter para 'nombre'
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Setter para 'nombre'
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
